7b351593212b81a3784de8ed07ad5cae3cbe7f79
JJTeo96 <teochunkit96@hotmail.com>
2019-06-22 01:33:59 +0800

    log

---
+++ admin_login/authentication.php
@@ -0,0 +1,89 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
+
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$adminName = $adminPassword = "";
+$username_err = $password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["adminName"]))){
+        $username_err = "<b><p style='color:red;'>Please enter username.</style></b>";
+    } else{
+        $adminName = trim($_POST["adminName"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["adminPassword"]))){
+        $password_err = "<b><p style='color:red;'>Please enter your password.</style></b>";
+    } else{
+        $adminPassword = trim($_POST["adminPassword"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT adminID, adminName, adminPassword FROM admin WHERE adminName = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $adminName;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $adminID, $adminName, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($adminPassword, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["adminID"] = $adminID;
+                            $_SESSION["adminName"] = $adminName;                            
+                            
+                            // Redirect user to welcome page
+                            header("location: ../admin/index.php");
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "<b><p style='color:red;'>The password you entered was not valid.</style></b>";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = "<b><p style='color:red;'>No account found with that username.</style></b>";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
---
+++ admin_login/register.php
@@ -0,0 +1,135 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$adminName = $adminPassword = $confirm_password = "";
+$adminName_err = $adminPassword_err = $confirm_password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Validate username
+    if(empty(trim($_POST["adminName"]))){
+        $adminName_err = "Please enter a username.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT adminID FROM admin WHERE adminName = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = trim($_POST["adminName"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $adminName_err = "This username is already taken.";
+                } else{
+                    $adminName = trim($_POST["adminName"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Validate password
+    if(empty(trim($_POST["adminPassword"]))){
+        $adminPassword_err = "Please enter a password.";     
+    } elseif(strlen(trim($_POST["adminPassword"])) < 6){
+        $adminPassword_err = "Password must have atleast 6 characters.";
+    } else{
+        $adminPassword = trim($_POST["adminPassword"]);
+    }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($adminPassword_err) && ($adminPassword != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($adminName_err) && empty($adminPassword_err) && empty($confirm_password_err)){
+        
+        // Prepare an insert statement
+        $sql = "INSERT INTO admin (adminName, adminPassword) VALUES (?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            
+            // Set parameters
+            $param_username = $adminName;
+            $param_password = password_hash($adminPassword, PASSWORD_DEFAULT); // Creates a password hash
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){ 
+                // Redirect to login page
+                header("location: login.php");
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Sign Up</h2>
+        <p>Please fill this form to create an account.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($adminName_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="adminName" class="form-control" value="<?php echo $adminName; ?>">
+                <span class="help-block"><?php echo $adminName_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($adminPassword_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="adminPassword" class="form-control" value="<?php echo $adminPassword; ?>">
+                <span class="help-block"><?php echo $adminPassword_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-default" value="Reset">
+            </div>
+            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
