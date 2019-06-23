d8862c30b87db8603a3ac311b527af36eda4b411
JJTeo96 <teochunkit96@hotmail.com>
2019-06-23 18:37:32 +0800

    Fix Edit & Delete

---
+++ admin/food_management/delete.php
@@ -0,0 +1,7 @@
+<?php
+	include('conn.php');
+	$id=$_GET['id'];
+	mysqli_query($conn,"delete from food_details where foodID='$id'");
+	header('location:index.php');
+
+?>
--- admin/food_management/edit.php
+++ admin/food_management/edit.php
@@ -1,14 +1,14 @@
 <?php
 	include('conn.php');
 	
-	$foodID=$_GET['foodID'];
+	$id=$_GET['id'];
 	
 	$foodName=$_POST['foodName'];
 	$foodQuantity=$_POST['foodQuantity'];
 	$foodPrice=$_POST['foodPrice'];
 	$foodRemark=$_POST['foodRemark'];
 	
-	mysqli_query($conn,"update food_details set foodName='$foodName', foodQuantity='$foodQuantity', foodPrice='$foodPrice', foodRemark='$foodRemark' where foodID='$foodID'");
+	mysqli_query($conn,"update food_details set foodName='$foodName', foodQuantity='$foodQuantity', foodPrice='$foodPrice', foodRemark='$foodRemark' where foodID='$id'");
 	header('location:index.php');
 
 ?>
