35d91682fc008c40585067ed3560181750576e26
JJTeo96 <teochunkit96@hotmail.com>
2019-07-06 22:45:27 +0800

    Add Header

---
+++ user/header.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico"> -->
+
+    <title>Buffet Catering</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="vendor/custom/css/buttons.css" rel="stylesheet">
+    <link href="vendor/custom/css/style.css" rel="stylesheet">
+
+    <!-- Font Awesome JS -->
+    <script defer src="vendor/fontawesome/js/solid.js" ></script>
+    <script defer src="vendor/fontawesome/js/fontawesome.js"></script>
+    <link rel="stylesheet" href="vendor/fontawesome/css/fontawesome.min.css">
+
+  </head>
+
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="index.php">Buffet Catering</a>
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Menu</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Package</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About Us</a>
+            </li>
+          </ul>
+          <li id="order-button" class="no-bullets"><a href="#">
+            <div class="button-small button-3d button-rounded button">Order Buffet <i class="fas fa-truck"></i></div></a>
+          </li>
+        </div>
+      </nav>
+    </header>
