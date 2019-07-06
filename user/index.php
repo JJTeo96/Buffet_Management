f5520a4100ed9ce91a180e7053c68c1774972415
JJTeo96 <teochunkit96@hotmail.com>
2019-07-06 12:32:08 +0800

    Update index.php

--- user/index.php
+++ user/index.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -6,42 +5,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <!-- <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico"> -->
 
-    <title>Carousel Template for Bootstrap</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/carousel/">
+    <title>Buffet Catering</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="carousel.css" rel="stylesheet">
-  </head>
-  <body>
+    <!-- Custom CSS -->
+    <link href="vendor/custom/css/buttons.css" rel="stylesheet">
+    <link href="vendor/custom/css/style.css" rel="stylesheet">
 
+    <!-- Font Awesome JS -->
+    <script defer src="vendor/fontawesome/js/solid.js" ></script>
+    <script defer src="vendor/fontawesome/js/fontawesome.js"></script>
+    <link rel="stylesheet" href="vendor/fontawesome/css/fontawesome.min.css">
+
+  </head>
+
+  <body>
     <header>
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">Carousel</a>
+        <a class="navbar-brand" href="index.php">Buffet Catering</a>
+        
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
+              <a class="nav-link" href="#">Menu</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link disabled" href="#">Disabled</a>
+              <a class="nav-link" href="#">Promotion</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About Us</a>
             </li>
           </ul>
-          <form class="form-inline mt-2 mt-md-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
+          <li id="order-button" class="no-bullets"><a href="#" target="_blank">
+            <div class="button-small button-3d button-rounded button">Order Buffet <i class="fas fa-truck"></i></div></a>
+          </li>
         </div>
       </nav>
     </header>
