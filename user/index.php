954d5598aec94802b4e2c709dfd6c0c87f6ae854
JJTeo96 <teochunkit96@hotmail.com>
2019-07-06 22:45:30 +0800

    Update index.php

--- user/index.php
+++ user/index.php
@@ -1,59 +1,5 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <!-- <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico"> -->
-
-    <title>Buffet Catering</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="vendor/custom/css/buttons.css" rel="stylesheet">
-    <link href="vendor/custom/css/style.css" rel="stylesheet">
-
-    <!-- Font Awesome JS -->
-    <script defer src="vendor/fontawesome/js/solid.js" ></script>
-    <script defer src="vendor/fontawesome/js/fontawesome.js"></script>
-    <link rel="stylesheet" href="vendor/fontawesome/css/fontawesome.min.css">
-
-  </head>
-
-  <body>
-    <header>
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="index.php">Buffet Catering</a>
-        
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Menu</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Promotion</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About Us</a>
-            </li>
-          </ul>
-          <li id="order-button" class="no-bullets"><a href="#" target="_blank">
-            <div class="button-small button-3d button-rounded button">Order Buffet <i class="fas fa-truck"></i></div></a>
-          </li>
-        </div>
-      </nav>
-    </header>
-
+<?php include_once('header.php');?>
+    
     <main role="main">
 
       <!-- carousel -->
@@ -98,6 +44,7 @@
   </div>
 </div>
  <!-- carousel -->
+ 
       <!-- Marketing messaging and featurettes
       ================================================== -->
       <!-- Wrap the rest of the page in another container to center all the content. -->
