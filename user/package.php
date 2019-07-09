e1d4c393ef4360ab26cb2bef675d6a9b7b04eaf3
JJTeo96 <teochunkit96@hotmail.com>
2019-07-09 17:43:36 +0800

    Add card

--- user/package.php
+++ user/package.php
@@ -1,1 +1,16 @@
- 
+<?php include_once('header.php');?>
+
+<div class="container" style="margin-top:6%">
+    <h1>Standard Buffet Packages</h1>
+    <span>Choose from an extensive selection of buffet catering package that’s suitable for your budget, catered to all your celebrations</span>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    
