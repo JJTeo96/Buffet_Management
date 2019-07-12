be884faea91d6716e7c3f51bfb0e3872aca49942
JJTeo96 <teochunkit96@hotmail.com>
2019-07-12 15:07:47 +0800

    Update package.php

--- user/package.php
+++ user/package.php
@@ -6,13 +6,14 @@
     <span>Choose from an extensive selection of buffet catering package that’s suitable for your budget, catered to all your celebrations</span>
 </div>
 
-<div class="container" style="margin-top:2%">
+<div class="container" style="margin-top:3%">
     
-    <div class="card" style="width: 18rem;">
+    <div class="card" style="width: 20rem;">
     <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg" class="card-img-top" alt="...">
     <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h5 class="card-title">Package Set A</h5>
+        <p class="card-text">8 Courses | min 20 Pax</p>
+        <p class="card-text">RM 15.00 <sup>/PAX</sup></p>
         <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
     </div>
