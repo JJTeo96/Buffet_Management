bc2faf17a1d2cf3267f41edd891552d78d0808f2
JJTeo96 <teochunkit96@hotmail.com>
2019-07-13 23:47:21 +0800

    Update profile.php
    view profile function

--- user/profile.php
+++ user/profile.php
@@ -17,69 +17,60 @@
                 
 		            <div class="row">
 		                <div class="col-md-12">
-		                    <form>
+                    <?php
+                      include_once '../config/database.php';
+                      $Drop=$_SESSION["userName"];
+                      $sql= "SELECT * FROM user where userName='$Drop';";
+                      $result = $db->query($sql);
+                      foreach ($result as $value){
+                    
+                    ?>
+		                    <form method="POST" action="action.php">
                               <div class="form-group row">
-                                <label for="username" class="col-4 col-form-label">User Name*</label> 
+                                <label for="username" class="col-4 col-form-label">User Name</label> 
                                 <div class="col-8">
-                                  <input id="username" name="username" placeholder="Username" class="form-control here" required="required" type="text" value="<?php echo $_SESSION['userName'] ?>">
+                                  <input id="username" name="username" placeholder="Username" class="form-control here"  type="text" value="<?php echo $value['userName'] ?>">
                                 </div>
                               </div>
                               <div class="form-group row">
-                                <label for="name" class="col-4 col-form-label">First Name</label> 
+                                <label for="name" class="col-4 col-form-label">IC</label> 
                                 <div class="col-8">
-                                  <input id="name" name="name" placeholder="First Name" class="form-control here" type="text">
+                                  <input id="" name="userIC" placeholder="IC" class="form-control here" type="text" value="<?php echo $value['userIC'] ?>">
                                 </div>
                               </div>
                               <div class="form-group row">
-                                <label for="lastname" class="col-4 col-form-label">Last Name</label> 
+                                <label for="lastname" class="col-4 col-form-label">Email</label> 
                                 <div class="col-8">
-                                  <input id="lastname" name="lastname" placeholder="Last Name" class="form-control here" type="text">
+                                  <input id="" name="userEmail" placeholder="Email" class="form-control here" type="email" value="<?php echo $value['userEmail'] ?>">
                                 </div>
                               </div>
                               <div class="form-group row">
-                                <label for="text" class="col-4 col-form-label">Nick Name*</label> 
+                                <label for="text" class="col-4 col-form-label">Address</label> 
                                 <div class="col-8">
-                                  <input id="text" name="text" placeholder="Nick Name" class="form-control here" required="required" type="text">
+                                  <textarea id="" name="userAddress" placeholder="Address" class="form-control here"  type="text"><?php echo $value['userAddress'] ?></textarea>
                                 </div>
                               </div>
                               <div class="form-group row">
-                                <label for="select" class="col-4 col-form-label">Display Name public as</label> 
+                                <label for="email" class="col-4 col-form-label">Contact 1</label> 
                                 <div class="col-8">
-                                  <select id="select" name="select" class="custom-select">
-                                    <option value="admin">Admin</option>
-                                  </select>
+                                  <input id="" name="userContact1" placeholder="Contact 1" class="form-control here"  type="text" value="<?php echo $value['userContact1'] ?>">
                                 </div>
                               </div>
                               <div class="form-group row">
-                                <label for="email" class="col-4 col-form-label">Email*</label> 
+                                <label for="website" class="col-4 col-form-label">Contact 2</label> 
                                 <div class="col-8">
-                                  <input id="email" name="email" placeholder="Email" class="form-control here" required="required" type="text">
+                                  <input id="" name="userContact2" placeholder="Contact 2" class="form-control here" type="text" value="<?php echo $value['userContact2'] ?>">
                                 </div>
                               </div>
-                              <div class="form-group row">
-                                <label for="website" class="col-4 col-form-label">Website</label> 
-                                <div class="col-8">
-                                  <input id="website" name="website" placeholder="website" class="form-control here" type="text">
-                                </div>
-                              </div>
-                              <div class="form-group row">
-                                <label for="publicinfo" class="col-4 col-form-label">Public Info</label> 
-                                <div class="col-8">
-                                  <textarea id="publicinfo" name="publicinfo" cols="40" rows="4" class="form-control"></textarea>
-                                </div>
-                              </div>
-                              <div class="form-group row">
-                                <label for="newpass" class="col-4 col-form-label">New Password</label> 
-                                <div class="col-8">
-                                  <input id="newpass" name="newpass" placeholder="New Password" class="form-control here" type="text">
-                                </div>
-                              </div> 
                               <div class="form-group row">
                                 <div class="offset-4 col-8">
-                                  <button name="submit" type="submit" class="btn btn-primary">Update My Profile</button>
+                                  <button name="update" type="submit" class="btn btn-primary">Save</button>
                                 </div>
                               </div>
                             </form>
+
+                            <?php } ?>
+
 		                </div>
 		            </div>
 		            
