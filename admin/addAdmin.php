9edd7f7e9035c162ec5d04f434f13ecd474edbfe
JJTeo96 <teochunkit96@hotmail.com>
2019-07-14 01:13:04 +0800

    Create addAdmin.php

---
+++ admin/addAdmin.php
@@ -0,0 +1,11 @@
+<?php 
+    //create an array to set page-level variables
+    $page=array();
+    $page['title']='Add Admin';
+    /*once the file is impoted, the variables set above will become available to it*/
+
+    //include the page header
+    include('header.php');
+?>
+
+<?php include('../admin_login/register.php');?>
