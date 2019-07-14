8e7f52c5d3977181733018a73b56b6b33df5986f
JJTeo96 <teochunkit96@hotmail.com>
2019-07-14 22:21:46 +0800

    Add package file

---
+++ admin/package_management/addnew.php
@@ -0,0 +1,13 @@
+<?php
+	include('conn.php');
+	
+	$foodName=$_POST['foodName'];
+	$foodQuantity=$_POST['foodQuantity'];
+	$foodPrice=$_POST['foodPrice'];
+	$foodRemark=$_POST['foodRemark'];
+	
+	
+	mysqli_query($conn,"insert into food_details (foodName, foodQuantity, foodPrice, foodRemark) values ('$foodName', '$foodQuantity', '$foodPrice', '$foodRemark')");
+	header('location:index.php');
+
+?>
