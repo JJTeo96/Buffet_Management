10dbf54bf2fb8c318c9cd93c8f6a1118ab774468
JJTeo96 <teochunkit96@hotmail.com>
2019-07-15 02:22:43 +0800

    Update package

--- user/package.php
+++ user/package.php
@@ -7,15 +7,42 @@
 </div>
 
 <div class="container" style="margin-top:3%">
-    
+<div class="row" style="margin: 0 auto">
+<div class="col-sm-4">
     <div class="card" style="width: 20rem;">
-    <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg" class="card-img-top" alt="...">
+    <img src="vendor/img/cardpic.png" class="card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title">Package Set A</h5>
-        <p class="card-text">8 Courses | min 20 Pax</p>
+        <p class="card-text">8 Courses | min 30 Pax</p>
         <p class="card-text">RM 15.00 <sup>/PAX</sup></p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <a href="#" class="btn btn-primary">View Details</a>
     </div>
     </div>
-    
 </div>
+
+<div class="col-sm-4">
+    <div class="card" style="width: 20rem;">
+    <img src="vendor/img/cardpic.png" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Package Set B</h5>
+        <p class="card-text">5 Courses | min 20 Pax</p>
+        <p class="card-text">RM 25.00 <sup>/PAX</sup></p>
+        <a href="#" class="btn btn-primary">View Details</a>
+    </div>
+    </div>
+</div>
+
+<div class="col-sm-4">
+    <div class="card" style="width: 20rem;">
+    <img src="vendor/img/cardpic.png" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Package Set C</h5>
+        <p class="card-text">11 Courses | min 20 Pax</p>
+        <p class="card-text">RM 20.00 <sup>/PAX</sup></p>
+        <a href="#" class="btn btn-primary">View Details</a>
+    </div>
+    </div>
+</div>    
+    
+</div> <!--Close row-->
+</div><!--Close container-->
