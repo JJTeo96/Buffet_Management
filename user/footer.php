2cd6f3d4ec8feadf0e8b8b81489eba873daf9b6c
JJTeo96 <teochunkit96@hotmail.com>
2019-11-29 17:17:38 +0800

    Update footer.php

--- user/footer.php
+++ user/footer.php
@@ -1,4 +1,14 @@
-<footer class="container" style="margin-top:5%">
-    <p class="float-right"><a href="#">Back to top</a></p>
-    <p>&copy; 2019-2019 By JJ</p>
-</footer>
+<style>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+}
+</style>
+
+<div class="footer">
+    <p class="float-right" style="margin-right:10%"><a href="#">Back to top</a></p>
+    <p style="margin-left:10%">&copy; 2019-2019 By JJ</p>
+</div>
+
