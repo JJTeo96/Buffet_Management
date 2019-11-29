778598278dca05100e5002d09397f3c1e90a2940
JJTeo96 <teochunkit96@hotmail.com>
2019-11-29 23:54:48 +0800

    Update footer.php

--- user/footer.php
+++ user/footer.php
@@ -1,4 +1,4 @@
-<footer class="container" style="margin-top:5%">
+<footer class="container" style="margin-top:10%">
     <p class="float-right"><a href="#">Back to top</a></p>
     <p>&copy; 2019-2019 By JJ</p>
 </footer>
