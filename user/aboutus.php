7d9e2ed560bb8e8ef0eb37bf6a799cb9bb436739
JJTeo96 <teochunkit96@hotmail.com>
2019-11-29 17:17:34 +0800

    Create aboutus.php

---
+++ user/aboutus.php
@@ -0,0 +1,34 @@
+<?php include_once('header.php');?>
+<style>
+.hero-image {
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(102, 100, 100, 0.5)), url("vendor/assets/img/Low-Res.jpg");
+    height: 40%;
+    width: 98%;
+    margin-left: 1%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+  }
+</style>
+<!-- Hero Image -->
+<div class="hero-image">
+  <div class="hero-text">
+    <!-- <h1 style="font-size:50px;color:white"><b>FANTASTIC FOOD FABULOUSLY PLANNED</b></h1>
+    <p style="color:white">FOR YOUR TUMMY & YOUR HEART</p>
+    <a href="package.php"><button>VIEW MENU</button></a> -->
+  </div>
+</div>
+<!-- End Hero Image -->
+
+<section id="page-title" style="margin-top:3%">
+    <div class="container clearfix">
+      <h1>About Us</h1>
+        <span>Offering a wide range of quality halal Asian Cuisine, SpoonFedNYC aims to satisfy your dining needs in 
+        every way you can imagine. We understand the importance of all events and occasions, and therefore, 
+        strive for perfection according to customers’ budget and requests. With a wealth of catering experience, 
+        SpoonFedNYC believes in delivering the very best with a touch of class and originality.</span> 
+    </div>
+</section>
+
+<?php include_once('footer.php');?>
