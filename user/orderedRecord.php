153ae9f184cc11d31a995029e0b34fd85b67416a
JJTeo96 <teochunkit96@hotmail.com>
2019-11-30 23:58:00 +0800

    Update orderedRecord.php
    add table

--- user/orderedRecord.php
+++ user/orderedRecord.php
@@ -1,33 +1,69 @@
 <?php include('header.php'); ?>
+
+<!--Head-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<!-- <link rel="stylesheet" href="../vendor/bootstrap/3.3.6/css/bootstrap.min.css" /> -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- Material Design Bootstrap -->
+<!-- <link href="vendor/assets/MDB/css/mdb.min.css" rel="stylesheet">
+<link href="vendor/assets/MDB/css/style.css" rel="stylesheet"> -->
+
+<form method="POST" action="promotion.php">
 <div class="container">
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+	<div style="height:50px;"></div>
+	<div class="well" style="margin:auto; padding:auto; width:80%;">
+	<h1 style="text-align:center">Order History</h1>
+		<div style="height:50px;"></div>
+		<table class="table table-striped table-bordered table-hover" id="table_id">
+			<thead>
+				<th>Package Name</th>
+				<th>Number of Pax</th>
+				<th>Total Price</th>
+				<th>Invoice No</th>
+				<th>Bill Date</th>
+				<th>Action</th>
+			</thead>
+            
+			<tbody>
+			<?php
+				include('../config/database.php');
+        $userID=$_SESSION['userID'];
+        $query=mysqli_query($db,"select * from `invoice`
+        LEFT JOIN package ON package.package_id=invoice.packageID 
+        LEFT JOIN payment ON payment.invoice_id=invoice.invoice_id
+        WHERE userID='$userID'");
+				while($row=mysqli_fetch_array($query)){
+					?>
+					<tr>
+						<td><?php echo $row['package_name']; ?></td>
+						<td><?php echo $row['num_pax']; ?></td>	
+						<td><?php echo $row['price']; ?></td>	
+						<td><?php echo $row['invoice_id']; ?></td>	
+						<td><?php echo $row['createDate']; ?></td>	
+						<td><input type="button" name="getCp" value="action" class="btn btn-info"/></td>	
+					</tr>
+					<?php
+				}
+			
+			?>
+			</tbody>
+		</table>
+	</div>
 </div>
+</form>
+
+<script src="https:////cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+
+<script>
+    $(document).ready( function () {
+        $('#table_id').DataTable();
+    } );
+</script>
+
+</body>
+
+<?php include('footer.php'); ?>
+
+</html>
